ef09e56cf5fffd7787e5809a65b41b61d46b8b9a
mundembe <61733352+mundembe@users.noreply.github.com>
2023-09-23 13:49:52 +0200

    Introductory Console App

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld;
+
+public class HelloWorld {
+    public static void Main(string[] args){
+        Console.WriteLine("Hello World");
+        promptUserDetails();
+    }
+
+    public static void promptUserDetails(){
+        Console.WriteLine("Please Enter Your Name:");
+        var name = Console.ReadLine();
+        var date = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello {name}, on {date:d} at {date:t}");
+        Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey();
+    }
+}
